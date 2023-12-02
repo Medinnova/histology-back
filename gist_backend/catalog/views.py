f5e4346322d1f1ff48464f93ec1af41ff26f6bc4
XcenaX <vlad-057@maul.ru>
2023-12-02 19:17:34 +0600

    little fix

--- gist_backend/catalog/views.py
+++ gist_backend/catalog/views.py
@@ -228,7 +228,7 @@ class CreateSectionView(APIView):
         if not user:
             return Response({"error": "No user found with given token"}, status=401)          
 
-        if not section.owners.contains(user):
+        if section and not section.owners.contains(user):
             return Response({"error": "You hasve no access to this Section!"}, status=409)   
 
         user_sections = Category.objects.filter(owners__id=user.id)
