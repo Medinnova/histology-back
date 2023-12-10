4e0be71c233f87152e135c6b0f93daba58176a83
XcenaX <vlad-057@maul.ru>
2023-12-10 14:13:31 +0600

    added role view in admin

--- gist_backend/users/admin.py
+++ gist_backend/users/admin.py
@@ -1,47 +1,29 @@
 from django import forms
 from django.contrib import admin
 from .models import User, Confirmation
-from users.forms import UserAdminForm
+from users.forms import CustomUserChangeForm, UserAdminForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from rolepermissions.roles import get_user_roles, assign_role, clear_roles, AbstractUserRole
 from users.roles import *
 
 # admin.site.register(User, UserAdmin)
 @admin.register(User)
-class SectionAdmin(admin.ModelAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        # Форма для создания пользователя
-        form.base_fields["username"].label = "Имя пользователя"
-        form.base_fields['password'].label = "Пароль"
-        form.base_fields['is_staff'].label = "Редактор [Поставить галочку]"
-        form.base_fields['date_joined'].label = "Зарегистрирован"
-        form.base_fields['is_superuser'].label = 'Суперпользователь'
-        form.base_fields['email'].label = 'Почта'
-        form.base_fields['first_name'].label = 'Имя'
-        form.base_fields['last_name'].label = 'Фамилия'
-        form.base_fields['surname'].label = 'Отчество'
-        form.base_fields['phone_number'].label = 'Номер телефона'
-        # Необязательные поля
-        form.base_fields['email'].required = False
-        form.base_fields['first_name'].required = False
-        form.base_fields['last_name'].required = False
-        form.base_fields['surname'].required = False
-        form.base_fields['phone_number'].required = False
-        form.base_fields['university'].required = False
-        form.base_fields['subscription'].required = False
+class SectionAdmin(BaseUserAdmin):
+    # def get_form(self, request, obj=None, **kwargs):
+    #     form = super().get_form(request, obj, **kwargs)
 
-        form.base_fields['role'] = forms.ChoiceField(
-            choices=[(role.get_name(), role.get_name()) for role in AbstractUserRole.__subclasses__()],
-            required=False
-        )
-        if obj:  # Проверка, что это не новый объект
-            user_roles = get_user_roles(obj)
-            print("ROLES : ", user_roles)
-            if user_roles:
-                form.base_fields['role'].initial = user_roles[0].get_name()
+    #     form.base_fields['role'] = forms.ChoiceField(
+    #         choices=[(role.get_name(), role.get_name()) for role in AbstractUserRole.__subclasses__()],
+    #         required=False
+    #     )
 
-        return form
+    #     if obj:  # Проверка, что это не новый объект
+    #         user_roles = get_user_roles(obj)
+    #         if user_roles:
+    #             form.base_fields['role'].initial = user_roles[0].get_name()
+
+    #     return form
+    form = CustomUserChangeForm
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
@@ -51,16 +33,17 @@ class SectionAdmin(admin.ModelAdmin):
             clear_roles(obj)
             assign_role(obj, form.cleaned_data['role'])
 
+    fieldsets = (
+        ('Личные данные', {'fields': ('username', 'password', 'first_name', 'last_name', 'surname', 'email')}),
+        ('Права доступа', {'fields': ('is_staff', 'is_superuser', 'role')}),
+        ('Дополнительная информация', {'fields': ('phone_number', 'university', 'subscription', 'date_joined')}),
+    )
+
     # Кортеж столбцов, отображаемых в списке пользователей
-    list_display = ('username_display', 'email_display', 'is_staff_display',
-                    'is_superuser_display', 'date_joined_display', 'id_display', 'university_display', 'subscription_display')
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'date_joined')
 
-    # Настройка фильтра для поиска по полю username
+    # Настройка фильтра для поиска
     search_fields = ('username__startswith',)
-
-    # Поля для добавления нового пользователя
-    fields = ('username', 'password', 'is_staff', 'date_joined', 'is_superuser', 'email', 'first_name', 'last_name',
-              'surname', 'phone_number', 'university', 'subscription')
 
     # Фильтры
     list_filter = ('is_staff', 'is_superuser', 'is_active')
--- gist_backend/users/forms.py
+++ gist_backend/users/forms.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from rolepermissions.roles import get_user_roles, assign_role, clear_roles
 from rolepermissions.roles import AbstractUserRole
+from django.contrib.auth.forms import UserChangeForm
 from users.roles import *
 
 class UserAdminForm(forms.ModelForm):
@@ -25,3 +26,20 @@ class UserAdminForm(forms.ModelForm):
             clear_roles(user)
             assign_role(user, self.cleaned_data['role'])
         return user
+
+class CustomUserChangeForm(UserChangeForm):
+    role = forms.ChoiceField(
+        choices=[(role.get_name(), role.get_name()) for role in AbstractUserRole.__subclasses__()],
+        required=False
+    )
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            user_roles = get_user_roles(self.instance)
+            if user_roles:
+                self.fields['role'].initial = user_roles[0].get_name()
